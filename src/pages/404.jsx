30ab5bfa1e0ffd82095ad8bf63345029c020fde7
Adam Morsi <adam.morsi@yahoo.com>
2021-02-05 17:28:26 +0200

    add custom not found page

---
+++ src/pages/404.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+//styles
+import styles from '../styles/pages/404.module.scss';
+
+const NotFound = () => {
+	return <div>404 Not found!</div>;
+};
+
+export default NotFound;
